dff513aa86e20a9ee08e0dd51306b34b3d24874b
Ssuminn <wert159311@gmail.com>
2024-03-27 21:29:56 +0900

    :sparkles: 주소 생성 기능

--- src/main/java/org/example/tentrilliondollars/address/controller/AddressController.java
+++ src/main/java/org/example/tentrilliondollars/address/controller/AddressController.java
@@ -1,14 +1,27 @@
 package org.example.tentrilliondollars.address.controller;
 
-
 import lombok.RequiredArgsConstructor;
+import org.example.tentrilliondollars.address.dto.AddressRequestDto;
 import org.example.tentrilliondollars.address.service.AddressService;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.tentrilliondollars.global.security.UserDetailsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users/address")
 public class AddressController {
 
     private final AddressService addressService;
 
+    @PostMapping
+    public ResponseEntity<String> createAddress(
+            @RequestBody AddressRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        addressService.createAddress(requestDto, userDetails.getUser());
+
+        return ResponseEntity.ok("주소 생성 완료");
+    }
 }
--- src/main/java/org/example/tentrilliondollars/address/service/AddressService.java
+++ src/main/java/org/example/tentrilliondollars/address/service/AddressService.java
@@ -1,7 +1,11 @@
 package org.example.tentrilliondollars.address.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.tentrilliondollars.address.dto.AddressRequestDto;
+import org.example.tentrilliondollars.address.entity.Address;
 import org.example.tentrilliondollars.address.repository.AddressRepository;
+import org.example.tentrilliondollars.user.entity.User;
+import org.example.tentrilliondollars.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +13,15 @@ import org.springframework.stereotype.Service;
 public class AddressService {
 
     private final AddressRepository addressRepository;
+    private final UserRepository userRepository;
+
+    public void createAddress(AddressRequestDto requestDto, User user) {
+        User finduser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        Address address = new Address(requestDto, finduser);
+
+        addressRepository.save(address);
+    }
 
 }
