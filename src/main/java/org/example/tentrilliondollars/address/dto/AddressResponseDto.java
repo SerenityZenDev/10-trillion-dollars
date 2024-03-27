92dfe7bfeec055ab1cad5773dfe6a719095b218d
Ssuminn <wert159311@gmail.com>
2024-03-27 21:43:18 +0900

    :bug: 주소 조회 null 해결

--- src/main/java/org/example/tentrilliondollars/address/dto/AddressResponseDto.java
+++ src/main/java/org/example/tentrilliondollars/address/dto/AddressResponseDto.java
@@ -1,14 +1,19 @@
 package org.example.tentrilliondollars.address.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.tentrilliondollars.address.entity.Address;
 
 @Getter
+@AllArgsConstructor
 public class AddressResponseDto {
     private String city;
     private String village;
     private String province;
 
     public AddressResponseDto(Address address) {
+        this.city = address.getCity();
+        this.village = address.getVillage();
+        this.province = address.getProvince();
     }
 }
