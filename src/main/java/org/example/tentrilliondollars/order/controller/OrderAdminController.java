4aff7a256ca13c87abbfeb3ededeec7967acff5d
tyler <smooky0702@gmail.com>
2024-03-28 15:56:43 +0900

    :sparkles: 주문 상태 기능 추가(@Secured 사용을 위에 securityConfig 수정),주문시 재고 변경 로직 추가,(dev pull후 테스트 완료)

--- src/main/java/org/example/tentrilliondollars/order/controller/OrderAdminController.java
+++ src/main/java/org/example/tentrilliondollars/order/controller/OrderAdminController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 public class OrderAdminController {
 
     private final OrderAdminService orderAdminService;
-
     @Secured("ROLE_SELLER")
     @PutMapping("/order/{orderId}")
     public ResponseEntity<CommonResponseDto> updateOrderState(@RequestBody ChangeStateDto requestDto, @PathVariable Long orderId){
