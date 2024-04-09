50d733e48094ceb4ff8683509e6bcce02f37f95c
JooJuneHo <jw059@naver.com>
2024-04-09 12:29:23 +0900

    :bug: JpaAuditing 적용

--- src/main/java/org/example/tentrilliondollars/global/config/JpaConfig.java
+++ src/main/java/org/example/tentrilliondollars/global/config/JpaConfig.java
@@ -3,8 +3,8 @@ package org.example.tentrilliondollars.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @Configuration
+@EnableJpaAuditing
 public class JpaConfig {
 
 }
